8de70b5f0f9bb5e6c6be61a3e6df8c16b172b176
Aum-kar <1977omkarpatil@gmail.com>
2024-12-23 19:43:35 +0530

    lombok

--- src/main/java/learn/springJpa/model/UserDetails.java
+++ src/main/java/learn/springJpa/model/UserDetails.java
@@ -2,57 +2,19 @@ package learn.springJpa.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
 @Entity
+@Data
+@NoArgsConstructor
 public class UserDetails {
     @Id private int uid;
     private String firstName;
     private String lastName;
     private String address;
-
-    public int getUid() {
-        return uid;
-    }
-
-    public void setUid(int uid) {
-        this.uid = uid;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public String toString() {
-        return "\nUserDetails{" +
-                "uid=" + uid +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
